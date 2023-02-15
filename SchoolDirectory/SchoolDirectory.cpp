b4662d4125b4a6e5697e338936bbbff2886e66ae
Andy Wagner <andygwagner@gmail.com>
2023-02-15 14:31:11 -0500

    Added lines to test createCourseList functions

--- SchoolDirectory/SchoolDirectory.cpp
+++ SchoolDirectory/SchoolDirectory.cpp
@@ -4,16 +4,14 @@
 #include "CreateCourseList.h"
 #include "Login.h"
 
-//using namespace System;
-//using namespace System::Data;
-//using namespace System::Data::SqlClient;
-//using namespace System::Collections::Generic;
-
 int main(array<System::String ^> ^args)
 {
 	Login tryLog;
+	CourseList tryCourseList;
 
 	String^ connString = "Data Source=.\\SQLEXPRESS;Initial Catalog=LeopardDatabase;Integrated Security=True";
+
+	tryCourseList.createCourseList(connString);
 	String^ userType = tryLog.checkUserType(connString);
 
 	if (userType != "") {
